2ff6eada97dd90366ba102ee6d3b75c725938eb2
Bee <mrhumanman2@gmail.com>
2022-02-06 01:17:41 -0500

    Add types to main

--- main.go
+++ main.go
@@ -8,3 +8,26 @@ import (
 	"github.com/gorilla/mux"
 	"golang.org/x/net/websocket"
 )
+
+type Player struct {
+	ID    string
+	Piece int
+	Board [10][20]int
+	Game  *Game
+}
+
+type Game struct {
+	CurrentID string
+	Players   *[]Player
+}
+
+type Games struct {
+	Lock    sync.Mutex
+	Games   *[]Game
+	Players *[]Player
+}
+
+type Client struct {
+	Conn *websocket.Conn
+	ID   string
+}
