9798e6f1b122d9afbadd6b7f249fa92a07abd247
Bee <mrhumanman2@gmail.com>
2022-02-06 01:18:12 -0500

    Add RandomString

--- utils/utils.go
+++ utils/utils.go
@@ -2,3 +2,14 @@ package utils
 
 import "math/rand"
 
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+
+func RandomString(length int) string {
+	runes := make([]rune, length)
+
+	for i := range runes {
+		runes[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(runes)
+}
