540a7f1f3c09bfd2076f40a61941b4e1a360de71
Bee <mrhumanman2@gmail.com>
2022-02-06 01:18:01 -0500

    Create basic http server

--- main.go
+++ main.go
@@ -31,3 +31,19 @@ type Client struct {
 	Conn *websocket.Conn
 	ID   string
 }
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/create-game", CreateGameHandler)
+	
+	server := &http.Server{
+		Handler: router,
+		Addr: "0.0.0.0:8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout: 15 * time.Second,
+	}
+
+	log.Println("Running on :8080")
+	log.Fatal(server.ListenAndServe())
+}
