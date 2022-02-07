d0749e53ef966099a285a5d172b9fb28803e783a
Bee Clark <mrhumanman2@gmail.com>
2022-02-06 19:18:38 -0500

    Too much

--- main.go
+++ main.go
@@ -4,46 +4,141 @@ import (
 	"log"
 	"net/http"
 	"sync"
+	"time"
 
+	"github.com/egggggg/detrista-concept/utils"
 	"github.com/gorilla/mux"
-	"golang.org/x/net/websocket"
+	"nhooyr.io/websocket"
 )
 
 type Player struct {
+	Board [10][20]int
+	Conn  *websocket.Conn
+	Game  *Game
 	ID    string
 	Piece int
-	Board [10][20]int
-	Game  *Game
 }
 
 type Game struct {
-	CurrentID string
-	Players   *[]Player
+	Current  int
+	Pieces   []int
+	PieceMap []string
+	Players  []*Player
+	Slug     string
 }
 
-type Games struct {
-	Lock    sync.Mutex
-	Games   *[]Game
-	Players *[]Player
+type Hub struct {
+	Count     int
+	Games     map[string]*Game
+	GamesMu   sync.Mutex
+	Players   map[string]*Player
+	PlayersMu sync.Mutex
 }
 
-type Client struct {
-	Conn *websocket.Conn
-	ID   string
+func (game *Game) AddPlayer(player *Player) {
+	game.Players = append(game.Players, player)
+	player.Game = game
 }
 
 func main() {
 	router := mux.NewRouter()
+	hub := Hub{}
 
-	router.HandleFunc("/create-game", CreateGameHandler)
+	router.HandleFunc("/create-game", CreateGame(&hub))
+	router.HandleFunc("/play/{slug}", JoinGame(&hub))
+	router.HandleFunc("/api/games/{slug}", HookGame(&hub))
 	
 	server := &http.Server{
-		Handler: router,
-		Addr: "0.0.0.0:8080",
+		Handler:      router,
+		Addr:         "0.0.0.0:8080",
 		WriteTimeout: 15 * time.Second,
-		ReadTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
 
 	log.Println("Running on :8080")
 	log.Fatal(server.ListenAndServe())
+}
+
+func CreateGame(hub *Hub) func(res http.ResponseWriter, req *http.Request) {
+	return func(res http.ResponseWriter, req *http.Request) {
+		slug := utils.RandomString(10)
+
+		game := &Game{
+			PieceMap: []string{"i", "l", "o", "s", "t", "j", "z"},
+			Slug:     slug,
+		}
+
+		counter := 0
+
+		hub.GamesMu.Lock()
+		defer hub.GamesMu.Unlock()
+
+		for hub.Games[slug] == nil {
+			if counter > 10 {
+				http.Error(res, "failed to create game, please try again", http.StatusInternalServerError)
+				return
+			}
+
+			game.Slug = utils.RandomString(10)
+			counter++
+		}
+
+		hub.Games[slug] = game
+	}
+}
+
+func JoinGame(hub *Hub) func(res http.ResponseWriter, req *http.Request) {
+	return func(res http.ResponseWriter, req *http.Request) {
+		slug := mux.Vars(req)["slug"]
+		if slug == "" {
+			http.Error(res, "page not found", http.StatusNotFound)
+			return
+		}
+
+		hub.GamesMu.Lock()
+		defer hub.GamesMu.Unlock()
+
+		if game := hub.Games[slug]; game == nil {
+			http.Error(res, "page not found", http.StatusNotFound)
+			return
+		}
+
+		http.ServeFile(res, req, "./pages/game.html")
+	}
+}
+
+func HookGame(hub *Hub) func(res http.ResponseWriter, req *http.Request) {
+	return func(res http.ResponseWriter, req *http.Request) {
+		slug := mux.Vars(req)["slug"]
+		if slug == "" {
+			http.Error(res, "page not found", http.StatusNotFound)
+			return
+		}
+
+		hub.Mu.Lock()
+		defer hub.Mu.Unlock()
+
+		game := hub.Games[slug]
+		if game == nil {
+			http.Error(res, "page not found", http.StatusNotFound)
+			return
+		}
+
+		id := utils.RandomString(20)
+
+		conn, err := websocket.Accept(res, req, nil)
+		if err != nil {
+			http.Error(res, "couldn't upgrade connection", http.StatusBadRequest)
+			return
+		}
+
+		player := &Player{
+			Conn: conn,
+			ID: id,
+		}
+
+		game.AddPlayer(player)
+
+
+	}
 }
